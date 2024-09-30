58a60db18d385cd41e07c8c239a4c0fa397336e6
mamontovia <Ivan.M.6@yandex.ru>
2024-09-30 11:54:44 +0400

    LR_2

--- main.py
+++ main.py
@@ -1,13 +1,134 @@
+import random
 import methods
+import numpy as np
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print("Часть 1")
+    s_matrix = np.array([[1, 0, 0, 1, 0, 1, 1],
+                         [1, 1, 0, 0, 0, 0, 1],
+                         [0, 0, 1, 1, 0, 0, 1],
+                         [1, 0, 1, 0, 1, 0, 1],
+                         [0, 0, 1, 1, 1, 1, 0]])
+    G = methods.RREF(methods.REF(s_matrix))
+    print("Порождающая матрица G:", G, sep = "\n")
+    G_standard = methods.standard_view(G)
+    print("Порождающая матрица G в стандартном виде:", G_standard, sep = "\n")
+    print()
 
-    # Пример
-    matrix = ([[1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1],
-               [0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0],
-               [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-               [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1],
-               [0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0],
-               [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0]]
-    )
-    methods.LinearCodeWithErrors(matrix)
+    H = methods.h_matrix(G_standard)
+    print("Проверочная матрица H:", H, sep = "\n")
+    print()
+
+    syndrome_table = methods.generate_syndrome_table(H, 1)
+    print("Таблица синдромов:", syndrome_table, sep = "\n")
+    print()
+
+    u = np.array([1, 0, 0, 1])
+    print("Кодовое слово длины k = 4:", u, sep = "\n")
+    v = u @ G_standard % 2
+    print("Отправленное кодовое слово длины n = 7:", v, sep = "\n")
+    error = np.array([0] * 7)
+    error[random.randint(0, 6)] = 1
+    print("Возникшая ошибка:", error, sep = "\n")
+    v = (v + error) % 2
+    print("Принятое с ошибкой слово:", v, sep = "\n")
+    syndrome = v @ H % 2
+    print("Синдром принятого сообщения:", syndrome, sep = "\n")
+    error = np.array([0] * 7)
+    error[syndrome_table[tuple(syndrome)][0]] = 1
+    v = (v + error) % 2
+    print("Исправленное сообщение:", v, sep = "\n")
+    print("Отправленное и исправленное сообщение совпадают")
+    print()
+
+    print("Кодовое слово длины k = 4:", u, sep = "\n")
+    print("Отправленное кодовое слово длины n = 7:", v, sep = "\n")
+    error = np.zeros(7, dtype = int)
+    a, b = random.sample(range(7), 2)
+    error[a], error[b] = 1, 1
+    print("Возникшая ошибка:", error, sep = "\n")
+    v = (v + error) % 2
+    print("Принятое с ошибкой слово:", v, sep = "\n")
+    syndrome = v @ H % 2
+    print("Синдром принятого сообщения:", syndrome, sep = "\n")
+    error = np.array([0] * 7)
+    error[syndrome_table[tuple(syndrome)][0]] = 1
+    v = (v + error) % 2
+    print("Исправленное сообщение:", v, sep = "\n")
+    print("Отправленное и исправленное сообщение не совпадают")
+    print()
+
+
+    print("2 часть")
+    G_standard = np.array([[1,0,0,0,1,1,1,1,0,0,0,0],
+                            [0,1,0,0,0,1,1,1,1,1,0,0],
+                            [0,0,1,0,1,0,0,1,1,1,1,0],
+                            [0,0,0,1,0,0,1,1,0,0,1,1]])
+    print("Порождающая матрица G в стандартном виде:", G_standard, sep = "\n")
+    print()
+
+    H = methods.h_matrix(G_standard)
+    print("Проверочная матрица H:", H, sep = "\n")
+    print()
+
+    syndrome_table = methods.generate_syndrome_table(H, 2)
+    print("Таблица синдромов:", syndrome_table, sep = "\n")
+    print()
+
+    u = np.array([0, 0, 1, 0])
+    print("Кодовое слово длины k = 4:", u, sep = "\n")
+    v = u @ G_standard % 2
+    print("Отправленное кодовое слово длины n = 12:", v, sep = "\n")
+    error = np.array([0] * 12)
+    error[random.randint(0, 11)] = 1
+    print("Возникшая ошибка:", error, sep = "\n")
+    v = (v + error) % 2
+    print("Принятое с ошибкой слово:", v, sep = "\n")
+    syndrome = v @ H % 2
+    print("Синдром принятого сообщения:", syndrome, sep = "\n")
+    error = np.array([0] * 12)
+    for index in syndrome_table[tuple(syndrome)]:
+        error[index] = 1
+    v = (v + error) % 2
+    print("Исправленное сообщение:", v, sep = "\n")
+    print("Отправленное и исправленное сообщение совпадают")
+    print()
+
+    print("Кодовое слово длины k = 4:", u, sep = "\n")
+    print("Отправленное кодовое слово длины n = 12:", v, sep = "\n")
+    error = np.array([0] * 12)
+    a, b = random.sample(range(12), 2)
+    error[a], error[b] = 1, 1
+    print("Возникшая ошибка:", error, sep = "\n")
+    v = (v + error) % 2
+    print("Принятое с ошибкой слово:", v, sep = "\n")
+    syndrome = v @ H % 2
+    print("Синдром принятого сообщения:", syndrome, sep = "\n")
+    error = np.array([0] * 12)
+    for index in syndrome_table[tuple(syndrome)]:
+        error[index] = 1
+    v = (v + error) % 2
+    print("Исправленное сообщение:", v, sep = "\n")
+    print("Отправленное и исправленное сообщение совпадают")
+    print()
+
+    print("Кодовое слово длины k = 4:", u, sep = "\n")
+    print("Отправленное кодовое слово длины n = 12:", v, sep = "\n")
+    error = np.array([0] * 12)
+    a, b, c = random.sample(range(12), 3)
+    error[a], error[b], error[c] = 1, 1, 1
+    print("Возникшая ошибка:", error, sep = "\n")
+    v = (v + error) % 2
+    print("Принятое с ошибкой слово:", v, sep = "\n")
+    syndrome = v @ H % 2
+    print("Синдром принятого сообщения:", syndrome, sep = "\n")
+    error = np.array([0] * 12)
+    if tuple(syndrome) in syndrome_table:
+        for index in syndrome_table[tuple(syndrome)]:
+            error[index] = 1
+        v = (v + error) % 2
+        print("Исправленное сообщение:", v, sep = "\n")
+        print("Отправленное и исправленное сообщение не совпадают")
+        print()
+    else:
+        print("Синдрома, соответствующего данной ошибке, не найдено в таблице синдромов. Сообщение исправить невозможно.")
--- methods.py
+++ methods.py
@@ -1,234 +1,99 @@
+import itertools
 import numpy as np
-from itertools import combinations
 
-# 1.1
-def ref(matrix):
+def REF(matrix: np.ndarray) -> np.ndarray:
+    matrix = matrix.copy()  # Создаем копию матрицы, чтобы не изменять оригинал
+    m, n = matrix.shape
+    current_row = 0
 
-    # преобразование матрицы в массив numpy
-    mat = np.array(matrix)
-    n_rows, n_cols = mat.shape
-    lead = 0
-    for r in range(n_rows):
-        if lead >= n_cols:
-            return mat
-        i = r
-        while mat[i, lead] == 0:
-            i += 1
-            if i == n_rows:
-                i, lead = r, lead + 1
-                if lead == n_cols:
-                    return mat
+    for col in range(n):
+        # Поиск строки с ненулевым элементом в текущем столбце
+        row_with_leading_one = current_row
+        while row_with_leading_one < m and matrix[row_with_leading_one, col] == 0:
+            row_with_leading_one += 1
 
-        # если необходимо, то меняем строки местами
-        mat[[i, r]] = mat[[r, i]]
+        if row_with_leading_one == m:
+            continue  # Нет ненулевых элементов в этом столбце, пропускаем
 
-        # обработка всех строк ниже текущей
-        for i in range(r + 1, n_rows):
-            if mat[i, lead] != 0:
-                mat[i] = (mat[i] + mat[r]) % 2
-        lead += 1
-    return mat
+        # Меняем строки местами, если ведущий элемент не в текущей строке
+        if row_with_leading_one != current_row:
+            matrix[[row_with_leading_one, current_row]] = matrix[[current_row, row_with_leading_one]]
 
+        # Обнуляем все элементы ниже ведущего
+        for row_below in range(current_row + 1, m):
+            if matrix[row_below, col] == 1:
+                matrix[row_below] ^= matrix[current_row]
 
-# 1.2
-def rref(mat):
-    mat = ref(mat)
-    n_rows, n_cols = mat.shape
+        current_row += 1
+        if current_row == m:
+            break
 
-    # проход по строкам сверху вниз
-    for r in range(n_rows - 1, -1, -1):
-
-        # находим ведущий элемент в строке
-        lead = np.argmax(mat[r] != 0)
-        if mat[r, lead] != 0:
-
-            # обнуляем все элементы, которые выше ведущего
-            for i in range(r - 1, -1, -1):
-                if mat[i, lead] != 0:
-                    mat[i] = (mat[i] + mat[r]) % 2
-    while not any(mat[n_rows - 1]):
-        mat, n_rows = mat[:-1, :], n_rows - 1
-    return mat
+    # Удаляем строки, состоящие только из нулей
+    non_zero_rows = np.any(matrix, axis=1)
+    return matrix[non_zero_rows]
 
 
-# 1.3: ведущие столбцы и удаление их для создания сокращённой матрицы
-def find_lead_columns(matrix):
-    lead_columns = []
-    for r in range(len(matrix)):
-        row = matrix[r]
-        for i, val in enumerate(row):
-            if val == 1:  # первый элемент 1 в строке - ведущий
-                lead_columns.append(i)
+def RREF(matrix: np.ndarray) -> np.ndarray:
+    matrix_copy = matrix.copy()  # Создаем глубокую копию матрицы, чтобы не изменять оригинал
+    m, n = matrix_copy.shape
+
+    # Идем снизу вверх по строкам
+    for current_row in range(m - 1, -1, -1):
+        leading_col = np.argmax(matrix_copy[current_row] != 0)
+        if matrix_copy[current_row, leading_col] == 0:
+            continue  # В строке только нули, пропускаем
+
+        # Обнуляем все элементы выше ведущего
+        for row_above in range(current_row):
+            if matrix_copy[row_above, leading_col] == 1:
+                matrix_copy[row_above] ^= matrix_copy[current_row]
+
+    return matrix_copy
+
+
+def standard_view(matrix: np.ndarray) -> np.ndarray:
+    matrix = matrix.copy()  # Копируем матрицу, чтобы не изменять оригинал
+    m, n = matrix.shape
+    identity_columns = []
+
+    # Находим позиции ведущих единичных элементов
+    for i in range(m):
+        for j in range(n):
+            if matrix[i, j] == 1 and np.all(matrix[:, j] == (np.eye(m)[i])):
+                identity_columns.append(j)
                 break
-    return lead_columns
+
+    # Проверяем, можно ли создать единичную матрицу из выбранных столбцов
+    if len(identity_columns) != m:
+        raise ValueError("Невозможно привести матрицу к стандартному виду")
+
+    # Создаем перестановку столбцов: сначала identity_columns, затем остальные
+    remaining_columns = [j for j in range(n) if j not in identity_columns]
+    permuted_columns = identity_columns + remaining_columns
+
+    # Применяем перестановку к матрице
+    return matrix[:, permuted_columns]
 
 
-# функция, удаляющая ведущие столбцы
-def remove_lead_columns(matrix, lead_columns):
-    mat = np.array(matrix)
-    reduced_matrix = np.delete(mat, lead_columns, axis = 1)
-    return reduced_matrix
+def h_matrix(matrix: np.ndarray) -> np.ndarray:
+    # Определяем размеры порождающей матрицы
+    m, n = matrix.shape
+    # Извлекаем дополнительную часть X (столбцы справа от единичной матрицы)
+    X = matrix[:, m:n]
+    identity_matrix = np.eye(n - m, dtype=int)
+    return np.vstack((X, identity_matrix))
 
 
-# Шаг 4: формирование матрицы H
-def form_H_matrix(X, lead_columns, n_cols):
+def generate_syndrome_table(matrix: np.ndarray, error_weight: int) -> dict:
+    n = matrix.shape[0]
+    syndrome_table = {}
+    for error in range(1, error_weight + 1):
+        for error_indices in itertools.combinations(range(n), error):
+            error_vector = np.zeros(n, dtype=int)
+            for index in error_indices:
+                error_vector[index] = 1
+            syndrome = error_vector @ matrix % 2
+            syndrome_table[tuple(map(int, syndrome))] = tuple(error_indices)
 
-    # инициализация единичной матрицы размером (n - k) на n
-    n_rows = np.shape(X)[1]
+    return syndrome_table
 
-    H = np.zeros((n_cols, n_rows), dtype = int)
-    I = np.eye(6, dtype = int)
-
-    H[lead_columns, :] = X
-    not_lead = [i for i in range(n_cols) if i not in lead_columns]
-    H[not_lead, :] = I
-
-    return H
-
-
-# основная функция для выполнения всех шагов
-def LinearCode(mat):
-
-    # 1.3.1: преобразование матрицы в ступенчатый вид
-    g_star = rref(mat)
-
-    print("G* (RREF матрица) =")
-    print(g_star)
-
-    # 1.3.2: найти ведущие столбцы
-    lead_columns = find_lead_columns(g_star)
-    print(f"lead = {lead_columns}")
-
-    # 1.3.3: удалить ведущие столбцы и получить сокращённую матрицу
-    X = remove_lead_columns(g_star, lead_columns)
-    print("Сокращённая матрица X =")
-    print(X)
-
-    # 1.3.4: сформировать проверочную матрицу H
-    n_cols = np.shape(mat)[1]
-    H = form_H_matrix(X, lead_columns, n_cols)
-    print("Проверочная матрица H =")
-    print(H)
-
-    return H
-
-
-# 1.3
-
-# функция для нахождения всех кодовых слов из порождающей матрицы
-def generate_codewords_from_combinations(G):
-    rows = G.shape[0]
-    codewords = set()
-
-    # перебираем все возможные комбинации строк матрицы G
-    for r in range(1, rows + 1):
-        for comb in combinations(range(rows), r):
-
-            # суммируем строки и добавляем результат в множество
-            codeword = np.bitwise_xor.reduce(G[list(comb)], axis = 0)
-            codewords.add(tuple(codeword))
-
-    # добавляем в множество нулевой вектор
-    codewords.add(tuple(np.zeros(G.shape[1], dtype = int)))
-
-    return np.array(list(codewords))
-
-# функция для умножения всех двоичных слов длины k на G
-def generate_codewords_binary_multiplication(G):
-    k = G.shape[0]
-    n = G.shape[1]
-    codewords = []
-
-    # генерируем все двоичные слова длины k
-    for i in range(2**k):
-        binary_word = np.array(list(np.binary_repr(i, k)), dtype = int)
-        codeword = np.dot(binary_word, G) % 2
-        codewords.append(codeword)
-
-    return np.array(codewords)
-
-# функция проверки кодового слова с помощью проверочной матрицы H
-def check_codeword(codeword, H):
-    return np.dot(codeword, H) % 2
-
-# функция вычисления кодового расстояния
-def calculate_code_distance(codewords):
-    min_distance = float('inf')
-
-    # подсчет количества ненулевых элементов для всех попарных разностей кодовых слов
-    for i in range(len(codewords)):
-        for j in range(i + 1, len(codewords)):
-            distance = np.sum(np.bitwise_xor(codewords[i], codewords[j]))
-            if distance > 0:
-                min_distance = min(min_distance, distance)
-
-    return min_distance
-
-# основная функция для выполнения всех шагов
-def LinearCodeWithErrors(mat):
-
-    # выполнение шагов, как и ранее
-    g_star = rref(mat)
-    lead_columns = find_lead_columns(g_star)
-    X = remove_lead_columns(g_star, lead_columns)
-    n_cols = np.shape(mat)[1]
-    H = form_H_matrix(X, lead_columns, n_cols)
-
-    print("G* (RREF матрица) =")
-    print(f"{g_star}\n")
-    print(f"lead = {lead_columns}\n")
-    print("Сокращённая матрица X =")
-    print(f"{X}\n")
-    print("Проверочная матрица H =")
-    print(f"{H}\n")
-
-    # 1.4.1: генерация всех кодовых слов через сложение строк
-    codewords_1 = generate_codewords_from_combinations(g_star)
-    print("Все кодовые слова (способ 1):")
-    print(f"{codewords_1}\n")
-
-    # 1.4.2: генерация кодовых слов умножением двоичных слов на G
-    codewords_2 = generate_codewords_binary_multiplication(g_star)
-    print("Все кодовые слова (способ 2):")
-    print(f"{codewords_2}\n")
-
-    # проверка, что множества кодовых слов совпадают
-    assert set(map(tuple, codewords_1)) == set(map(tuple, codewords_2)), "Наборы кодовых слов не совпадают!"
-
-    # проверка кодовых слов с помощью матрицы H
-    for codeword in codewords_1:
-        result = check_codeword(codeword, H)
-        assert np.all(result == 0), f"Ошибка: кодовое слово {codeword} не прошло проверку матрицей H"
-
-    print("Все кодовые слова прошли проверку матрицей H.")
-
-    # 1.4: вычисление кодового расстояния
-    d = calculate_code_distance(codewords_1)
-    t = 0
-    if t == 0:
-        t = 1
-    else:
-        t = (d - 1) // 2
-    print(f"Кодовое расстояние d = {d}")
-    print(f"Кратность обнаруживаемой ошибки t = {t}\n")
-
-    # проверка ошибки кратности t
-    e1 = np.zeros(n_cols, dtype = int)
-    e1[2] = 1                            # внесение ошибки в один бит
-    v = codewords_1[4]
-    print(f"e1 = {e1}")
-    print(f"v = {v}")
-    v_e1 = (v + e1) % 2
-    print(f"v + e1 = {v_e1}")
-    print(f"(v + e1)@H = {check_codeword(v_e1, H)} - error\n")
-
-    # проверка ошибки кратности t + 1
-    e2 = np.zeros(n_cols, dtype = int)
-    e2[6] = 1
-    e2[9] = 1                            # внесение ошибки в два бита
-    print(f"e2 = {e2}")
-    v_e2 = (v + e2) % 2
-    print(f"v + e2 = {v_e2}")
-    print(f"(v + e2)@H = {check_codeword(v_e2, H)} - no error")
-
-    return H
